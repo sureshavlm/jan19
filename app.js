e5d45ac6003f1ec59e09d2b39c513352cdbae1f5
suresh <suresh.avlm@gmail.com>
2019-01-20 09:45:57 +0530

    Initial checkins

---
+++ app.js
@@ -0,0 +1,21 @@
+/* main file */
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+	console.log('Server running on port %s', port);
+});
+
+/* http://localhost:8080 */
+app.get('/', function(req, res) {
+
+	res.sendFile(__dirname + '/index.html');
+
+});
+
+app.post('/login', function(req, res) {
+	
+	res.sendFile(__dirname + '/home.html');
+});
---
+++ util.js
@@ -0,0 +1,24 @@
+exports.getCountryCode = function(name) {
+
+	var countryCode = 0;
+
+	switch(name) {
+		case 'USA':
+			countryCode = '001';
+			break;
+		case 'India':
+			countryCode = 91;
+			break;
+
+		default: 
+			countryCode = 0;
+			break;
+	} 
+	return countryCode;
+};
+
+
+exports.getDate = function(format) {
+
+	return new Date();
+};
